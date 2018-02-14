6cb209f72d5426155938657e66868144cd10cdb4
Bgoldfa <bgoldfa@gap.com>
2018-02-14 09:29:13 -0500

    Second Commnit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-    <div>
-      
+    <div className = 'wrapper'>
+      <div className = "main-title">
+        <h1>Social-Scraper</h1>
+      </div>
+      <p> Please Entry your query param </p>
+     Search For: <input id="user-input-value" value = ''/>
+      <button id = 'submit-button'>  Submit </button>
     </div>
-    );
+    );  
   }
 }
 
